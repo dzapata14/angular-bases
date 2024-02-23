b0e36dc85f059264c52d667f8a880d5830160ce2
Daniel Zapata <danielzapata13@gmail.com>
2024-02-23 14:25:27 -0500

    Codigo sección 5

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,30 @@
+import { Character } from './../interfaces/character.interface';
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+  public characters: Character[] = [
+    {id:uuid(), name: 'Krillin', power:1000},
+    {id:uuid(), name:'Goku', power:9500},
+    {id:uuid(), name:'Vegeta', power:7500}
+  ];
+
+  public addCharacter = (character:Character):void => {
+    //this.characters.push({id: uuid(), name: character.name, power: character.power});
+    // or
+    this.characters.push({...character, id: uuid() });
+  }
+
+  /*public onDeleteCharacter = (index: number):void => {
+    this.characters.splice(index, 1);
+  }*/
+
+  public deleteCharacterById(id:string): void{
+    this.characters = this.characters.filter( character => character.id !== id);
+  }
+
+}
